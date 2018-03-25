c9d89416a4beb10ba949dc99cb41b8ae99d1514f
diego <diego@Andrey>
2018-03-25 12:57:35 -0600

    Primer Commit

---
+++ SpaceInvaders/desktop/src/com/andrews/spaceinvaders/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.andrews.spaceinvaders.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.andrews.spaceinvaders.GameMain;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameMain(), config);
+	}
+}
